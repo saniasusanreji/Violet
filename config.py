937176633d858f5455f8ae8cad3febad6e4c7a58
Sania Susan Reji <24cy492@mgits.ac.in>
2025-07-20 03:13:31 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-
-SPEAK_SPEED = 170  # Adjust voice speed
-CONFIDENCE_THRESHOLD = 0.5  # Object detection confidence threshold
-DETECTION_INTERVAL = 5  # Time between detections in seconds
